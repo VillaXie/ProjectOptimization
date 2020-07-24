133e96216aec2e3f59343fc6092c758c00236220
xieqiang <xieq0016@163.com>
2020-07-24 10:08:06 +0800

    首次提交.

---
+++ libnavannotation/src/main/java/com/study/libnavannotation/ActivityDestination.kt
@@ -0,0 +1,8 @@
+package com.study.libnavannotation
+
+@Target(AnnotationTarget.ANNOTATION_CLASS, AnnotationTarget.CLASS)
+annotation class ActivityDestination(
+    val pageUrl: String,
+    val needLogin: Boolean = false,
+    val asStarter: Boolean = false
+)
---
+++ libnavcompiler/src/main/java/com/study/libnavcompiler/MyClass.kt
@@ -0,0 +1,4 @@
+package com.study.libnavcompiler
+
+class MyClass {
+}
